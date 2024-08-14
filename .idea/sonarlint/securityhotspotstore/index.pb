d99d51e639c662ff4800da8cd85ee75bee6fa066
LeKheing <lekhangit1106@gmail.com>
2024-08-14 22:43:18 +0700

    something changes

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,12 @@
+
+h
+8src/main/java/com/kheing/todomanagement/entity/Todo.java,e\4\e47c2bfda0dc462da407063054481234d5452198
+:
+
+Dockerfile,6\6\6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+y
+Isrc/main/java/com/kheing/todomanagement/controller/GeneralController.java,a\8\a85fade1fd302ab72fbaf08c89c4b5787fbb0a08
+v
+Fsrc/main/java/com/kheing/todomanagement/TodoManagementApplication.java,b\3\b3480f9cf58b934495325f1f3b314b171df2f0be
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- todo-management/src/main/java/com/kheing/todomanagement/TodoManagementApplication.java
+++ todo-management/src/main/java/com/kheing/todomanagement/TodoManagementApplication.java
@@ -1,13 +1,30 @@
 package com.kheing.todomanagement;
 
+import com.kheing.todomanagement.entity.Todo;
+import com.kheing.todomanagement.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class TodoManagementApplication {
+	@Autowired
+	private TodoService todoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoManagementApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			todoService.addTodo(new Todo(1L,"Todo 1", LocalDate.now(),true));
+			todoService.addTodo(new Todo(2L,"Todo 2", LocalDate.now(),false));
+			todoService.addTodo(new Todo(3L,"Todo 2", LocalDate.now(),true));
+		};
+	}
 }
